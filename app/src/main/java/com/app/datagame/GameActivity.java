fe1045746f0a6ccd4155ab69c1a18bc7458e7036
jose-almir <mzt.jascfilho@voegol.com.br>
2024-11-11 13:58:27 -0300

    Commit to implementation final of the game

--- app/src/main/java/com/app/datagame/GameActivity.java
+++ app/src/main/java/com/app/datagame/GameActivity.java
@@ -2,7 +2,13 @@ package com.app.datagame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Range;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +16,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
+
+    private int [] diceeArray = {
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +42,31 @@ public class GameActivity extends AppCompatActivity {
 
         TextView textViewPlayerName = findViewById(R.id.txtPlayerName);
         Intent intent = getIntent();
-        textViewPlayerName.setText(intent.getStringExtra("PlayerName"));
+        String  fullName = "Olá ";
+        textViewPlayerName.setText(fullName.concat(intent.getStringExtra("PlayerName")).concat(" !"));
 
+        ImageView imageViewLeft = findViewById(R.id.imgLeft);
+        ImageView imageViewRight = findViewById(R.id.imgRight);
+
+        Button buttonRoll = findViewById(R.id.btnRoll);
+        buttonRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int diceRight = getNumber();
+                imageViewRight.setImageResource(diceeArray[diceRight]);
+
+                int diceLeft = getNumber();
+                imageViewLeft.setImageResource(diceeArray[diceLeft]);
+
+                String message = "Total: " + (diceRight + diceLeft + 2);
+
+                Toast.makeText(GameActivity.this, message , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private int getNumber() {
+        return new  Random().nextInt(6);
     }
 }
